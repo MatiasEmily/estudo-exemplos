8c12525bda8604665ca069af565dc0a800fee45c
Emily Matias <70017979+matiasemily@users.noreply.github.com>
2024-05-31 08:23:38 -0300

    Imprimindo comprimento e índices de array

---
+++ Python/desenvolvimentoextra1-2_109862.py
@@ -0,0 +1,17 @@
+"""
+Proz - Talento Cloud
+Introdução à Programação com foco em Front-End - Turma 28
+Preparação para HTML e CSS
+Desenvolvimento Extra 1.2 - 109862
+Emily Matias
+
+Enunciado:
+Copie o array apresentado embaixo no seu editor de código, e imprima no terminal: a quantidade de elementos que ele possui, o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+"""
+
+# Array de músicos
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+# Imprime o tamanho do array e índices 2, 9 e 14
+print(len(lista_musicos), lista_musicos[2], lista_musicos[9], lista_musicos[14])
