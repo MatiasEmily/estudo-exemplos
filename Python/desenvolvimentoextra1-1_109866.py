8d62747c38d5241723c074e2f5efab6aaba02cd7
Emily Matias <70017979+matiasemily@users.noreply.github.com>
2024-05-31 08:09:45 -0300

    Imprimindo arrays

---
+++ Python/desenvolvimentoextra1-1_109866.py
@@ -0,0 +1,21 @@
+"""
+Proz - Talento Cloud
+Introdução à Programação com foco em Front-End - Turma 28
+Preparação para HTML e CSS
+Desenvolvimento Extra 1.1 - 109866
+Emily Matias
+
+Enunciado:
+Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print().
+O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc).
+O segundo array deve conter os anos de nascimento de familiares e amigos seus.
+Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+"""
+
+# Itens de cura vendidos no jogo Yakuza
+suplementos = ['Toughness ZZ', 'Toughness Infinity', 'Tauriner ++', 'Staminan Royale', 'Staminan Spark']
+print(suplementos)
+
+# Anos de nascimento
+anos_nascimento = [1963, 1964, 1985, 1993, 1995]
+print(anos_nascimento)
