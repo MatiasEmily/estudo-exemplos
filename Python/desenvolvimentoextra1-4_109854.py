92b7f6f7cf1341fd0483fa2a0b3a946a70f92d78
Emily Matias <70017979+matiasemily@users.noreply.github.com>
2024-05-31 11:24:16 -0300

    Manipulando array com índice, pop() e append()

---
+++ Python/desenvolvimentoextra1-4_109854.py
@@ -0,0 +1,30 @@
+"""
+Proz - Talento Cloud
+Introdução à Programação com foco em Front-End - Turma 28
+Preparação para HTML e CSS
+Desenvolvimento Extra 1.4 - 109854
+Emily Matias
+
+Enunciado:
+A loja de cosméticos ficou muito feliz com seu trabalho e chamaram você novamente!
+Dessa vez, eles precisam que você atualize o array de produtos.
+Agora, eles estão vendendo rímel ao invés de batons, e cremes hidratantes no lugar de loções.
+Além disso, ficaram sem delineadores, então precisam que você remova ele da lista de produtos.
+Imprima a nova lista no terminal para verificar que as alterações foram realizadas corretamente.
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+Como desafio, adicione dois novos produtos da sua escolha à lista. 
+"""
+
+# Array de produtos
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+# Atualização de produtos
+lista_produtos[1] = 'rímel' # substitui batons
+lista_produtos[4] = 'cremes hidratantes' # substitui loções
+lista_produtos.pop() # remove último item da lista, delineadores
+lista_produtos.append('condicionadores') # adiciona condicionadores ao fim da lista
+lista_produtos.append('esfoliantes') # adiciona esfoliantes ao fim da lista
+
+# Laço para imprimir o array
+for produto in lista_produtos:
+  print(f"Temos {produto} à venda!")
